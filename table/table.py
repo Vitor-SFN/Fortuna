d3f3c0d69a239a3b5750d74633435e1748661bcf
Nick <nicolaspinheirogomes3@gmail.com>
2025-07-21 22:24:31 -0300

    fix: Removed unecessary method - table class

--- table/table.py
+++ table/table.py
@@ -1,10 +1,3 @@
 class Table:
     pot = 0
     players = []
-
-    #def __init__(NONE):
-    #    NONE
-
-    def add_players(self, *players):
-        for player in players:
-            Table.players.append(player)
